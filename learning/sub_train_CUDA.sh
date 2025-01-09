a78eec976bd540b887d8c0d5516044455d530575
BrettHoover-NOAA <Brett.Hoover@noaa.gov>
2025-01-09 18:32:24 -0500

    First version of CUDA-enabled SupErrNet training/validation, for v2.2

---
+++ learning/sub_train_CUDA.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+module load cuda
+
+runDir=`pwd`
+trainDir=${runDir}/training
+statsFile=/scratch1/NCEPDEV/da/Brett.Hoover/ML_AMVs/superob_stats.nc
+nEpochs=1
+annealing=0.25
+modelName=SupErrNet_v2.2.1_CUDA
+
+runTime=8:00:00
+
+####################################################################################
+# first epoch: 0
+# no prior model is loaded before training
+# no dependencies
+let epoch=0
+# generate jobName based on modelName and epoch
+jName_train=train_${modelName}_ep_${epoch}
+# submit job and retain jobID
+jid_train=$(sbatch --parsable -A da-cpu -N 1 -t ${runTime} -p fge -q gpuwf --job-name=${jName_train} run_train_SupErrNet-CUDA.sh ${trainDir} ${statsFile} ${epoch} ${annealing} ${runDir} ${modelName})
+####################################################################################
+# loop through epochs starting from one
+let epoch=1
+jid_prior=${jid_train}
+while [ ${epoch} -lt ${nEpochs} ]
+do
+    # generate jobID based on modelName and epoch
+    jName_train=train_${modelName}_ep_${epoch}
+    # submit job with jid_prior as a dependency
+    jid_train=$(sbatch --parsable -A da-cpu -N 1 -t ${runTime} -p fge -q gpuwf --dependency=afterany:${jid_prior} --job-name=${jName_train} run_train_SupErrNet-CUDA.sh ${trainDir} ${statsFile} ${epoch} ${annealing} ${runDir} ${modelName})
+    # increment epoch
+    ((epoch++))
+    # assign current jobID as jid_prior
+    jid_prior=${jid_train}
+done
+
